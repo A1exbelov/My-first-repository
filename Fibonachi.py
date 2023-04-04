e273bde75007bfa4bea7a38eafb70ed20a8058e9
A1exbelov <a1exandr00bel0v@gmail.com>
2023-04-05 00:39:46 +0400

    Adding the output of the full fibonachi sequence

--- Fibonachi.py
+++ Fibonachi.py
@@ -1,12 +1,15 @@
 def fib(n):
     if n == 0:
-        return 0
+        return []
     elif n == 1:
-        return 1
+        return [0]
     else:
-        return fib(n-1) + fib(n-2)
+        fib_seq = [0, 1]
+        for i in range(2, n):
+            next_num = fib_seq[i-1] + fib_seq[i-2]
+            fib_seq.append(next_num)
+        return fib_seq
 
 n = int(input("Введите число n: "))
-fib_n = fib(n)
-print(fib_n)
-
+fib_seq = fib(n)
+print(fib_seq)
